703967f97863c045b564aa4b4c88af51173515e4
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-28 00:18:18 +0700

    rerouting and layout concept

--- app.js
+++ app.js
@@ -3,35 +3,57 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 
 const app = express()
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
-let items =[]
+let items = []
+let workItems = []
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
   // alert("Hello world!")
   var date = new Date()
   let options = {
-    weekday : "long",
-    day : "numeric",
-    month : "long",
-    year : "numeric"
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
   }
   todayFullDate = date.toLocaleDateString("id-ID", options)
-  res.render("list", {listTitle : todayFullDate, addedItem : items})
+  res.render("list", {
+    listName: "General List",
+    addedItem: items,
+    todayDate : todayFullDate
+  })
 })
 
-app.post("/", function(req,res){
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listName: "Work List",
+    addedItem: workItems,
+    todayDate : todayFullDate
+  })
+})
+
+app.post("/", function(req, res) {
   let item = req.body.addedItem
-  if (item != ""){
-    items.push(item)
-  }else {
+  if (item != "") {
+    if (req.body.list === "Work") {
+      workItems.push(item)
+      res.redirect("/work")
+    } else if (req.body.list === "General") {
+      items.push(item)
+      res.redirect("/")
+    }
+  } else {
     console.log("No item")
   }
-  res.redirect("/")
+  console.log(req.body)
+
 })
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server ready gan!")
 })
