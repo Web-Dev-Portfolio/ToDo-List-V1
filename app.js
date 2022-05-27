26bc63220d9841c8a931c19f52bfe215a9a8c82e
fitransyah-sourcetree <fitransyah.rusman@gmail.com>
2022-05-27 23:01:56 +0700

    pusing post body to list

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
+let items =[]
+
 app.get("/", function(req,res){
   // alert("Hello world!")
-  res.render("list", {listTitle : "Testing title", addedItem : "Testing Item"})
+  res.render("list", {listTitle : "Testing title", addedItem : items})
 })
 
 app.post("/", function(req,res){
-
+  let item = req.body.addedItem
+  items.push(item)
+  res.redirect("/")
 })
 
 app.listen(3000, function(){
